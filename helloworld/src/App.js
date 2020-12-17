ecb9e0885de32b553e7fc82e6597c72f360b3082
jangle <shubhamjangale0000@gmail.com>
2020-12-17 08:34:26 +0530

    Added : Added code for event binding

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -6,14 +6,20 @@ class App extends React.Component{
   constructor(){
     super()
     this.state={
-      body: 'Hello All to BridgeLabz'
+      userName: 'Shubham'
     }
   }
+
+  onClick = ($event) => {
+    console.log("save button is clicked!" , $event);
+    window.open(this.url, "_blank");
+  }
+  
   render(){
     return(
       <div>
-        <h1>{this.state.body}</h1>
-        <img src ={logo} alt ="Logo" />
+        <h1>Hello {this.state.userName} to BridgeLabz</h1>
+        <img src ={logo} onClick={this.onClick} alt ="Logo" />
       </div>
     );
   }
