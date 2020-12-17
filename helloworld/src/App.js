58ffd66a8e638019d492099d5d761b1001e74a53
jangle <shubhamjangale0000@gmail.com>
2020-12-17 08:29:02 +0530

    Added : Added code to bind the properties

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/Bridgelabz.jpg';
 
 class App extends React.Component{
   constructor(){
     super()
     this.state={
-      body: 'Hello Shubham for Capgemini'
+      body: 'Hello All to BridgeLabz'
     }
   }
   render(){
     return(
       <div>
         <h1>{this.state.body}</h1>
+        <img src ={logo} alt ="Logo" />
       </div>
     );
   }
